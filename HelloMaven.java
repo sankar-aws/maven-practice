d73ead4a733a9ca02c2035b3e77122111062c7f7
sankar-aws <sankaranaws@gmail.com>
2025-11-02 13:40:48 +0530

    Update HelloMaven.java

--- HelloMaven.java
+++ HelloMaven.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloMaven {
     public static void main(String[] args) {
-        System.out.println("Hello from maven project!");
+        System.out.println("Hello from kube project!");
         System.out.println("Hello from Shankar");
         System.out.println("bye from Shankar");
     }
